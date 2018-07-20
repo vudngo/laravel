be890dae219fac15359413a1ec771bccd3586558
Neil Manvar <neil@sentry.io>
2018-07-20 15:22:58 -0700

    Add errors and css

---
+++ app/Http/Middleware/SentryContext.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class SentryContext
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (app()->bound('sentry')) {
+            /** @var \Raven_Client $sentry */
+            $sentry = app('sentry');
+
+            // Add user context
+            if (auth()->check()) {
+                $sentry->user_context(['email' => auth()->user()->email]);
+            } else {
+                $sentry->user_context(['id' => null]);
+            }
+        }
+
+        return $next($request);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/undefined_variable', function () {
+    $x = $abc;
+});
+
+Route::get('/divbyzero', function () {
+    1/0; // simulated error
+});
+
+Route::get('/badinclude', function () {
+    include('invalidfile');
+});
+
+Route::get('/outofrange', function () {
+    $cars = array("0", "1")[2];
+});
+
+Route::get('/invalidfunction', function () {
+    invalidFunction();
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/logout', 'Auth\LoginController@logout')->name('logout' );
